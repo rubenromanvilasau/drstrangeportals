6fdc5e183814c0b012195f49a03b887681cdce01
Ruben <ruben.roman@mayor.cl>
2022-04-27 23:32:23 -0400

    certificates and port changed

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ var http   = require('http');
 var https  = require('https');
 var path   = require("path");
 
-// Public Self-Signed Certificates for HTTPS connection
+// Certificates for HTTPS connection
 var privateKey  = fs.readFileSync('./../certificates/key.pem', 'utf8');
 var certificate = fs.readFileSync('./../certificates/cert.pem', 'utf8');
 
@@ -15,12 +15,14 @@ app.use(express.static(__dirname + '/public'));
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8443;
 
 // Allow access from all the devices of the network (as long as connections are allowed by the firewall)
 var LANAccess = "0.0.0.0";
+
 // For http
 //httpServer.listen(8080, LANAccess);
+
 // For https
 httpsServer.listen(port, LANAccess, ()=>{
     console.log('Server listening on port ' + port);
